fb2c9b24ed31bc39b428ff7b53d2051fae84c23b
madhureshChoudhary (minoshi) <minoshideveloper8@gmail.com>
2024-11-05 18:53:08 +0530

    updating

--- render.py
+++ render.py
@@ -1,1 +1,14 @@
-print("hello World!")
+#ssh 22f3000652@se2001.ds.study.iitm.ac.in
+
+import cv2
+import numpy as np
+
+
+resolution_x = 500
+resolution_y = 500
+
+pic = np.zeros((resolution_x, resolution_y))
+
+print(repr(pic))
+
+cv2.imwrite("picture2.png", pic)
